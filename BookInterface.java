67775ab607ac8613d3fb477549cf8e1d8fa09bee
rhtweedie <rhtweedie12@gmail.com>
2022-09-11 13:06:53 +0200

    Add HashMap and implement interface

--- BookInterface.java
+++ BookInterface.java
@@ -1,8 +1,12 @@
+import java.util.HashMap;
+
 interface BookInterface {
 
-    public void addBook();
+    HashMap<String, String> bookList = new HashMap<String, String>();
 
-    public void editBook();
+    public void addBook(String title, String author);
+
+    public void editBook(String oldTitle, String newTitle, String newAuthor);
 
     public String[] searchBooks();
 }
