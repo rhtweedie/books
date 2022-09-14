f22c39083e4400fdd46aa3ce2b609028712b5ae0
rhtweedie <rhtweedie12@gmail.com>
2022-09-14 23:30:44 +0100

    Add some JavaDoc

--- BookInterface.java
+++ BookInterface.java
@@ -1,10 +1,19 @@
 interface BookInterface {
 
+    /**
+     * Adds a new book to the database.
+     * 
+     * @param title  The title of the book
+     * @param author The author of the book
+     */
     public void addBook(String title, String author);
 
     public void editBook(String oldTitle, String newTitle, String newAuthor);
 
     public String[] searchBooks();
 
+    /**
+     * Gets all the books in the database.
+     */
     public Book[] getAllBooks();
 }
