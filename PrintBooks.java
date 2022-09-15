53e8ee12d85f3e8eccb2c5fd71a8dac58d41adc3
rhtweedie <rhtweedie12@gmail.com>
2022-09-15 22:31:42 +0100

    Replace array with ArrayList

--- PrintBooks.java
+++ PrintBooks.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class PrintBooks {
 
     public static void main(String[] args) {
         BookInterface database = new TextFileBookDatabase();
-        Book[] books = database.getAllBooks();
+        ArrayList<Book> books = database.getAllBooks(args[0]);
         System.out.println("--- All books contained in database ---");
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i].getTitle() + ", " + books[i].getAuthor());
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println((books.get(i)).getTitle() + ", " + (books.get(i)).getAuthor());
         }
     }
 }
