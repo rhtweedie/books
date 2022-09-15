78110bc94ed6406b76de11ad264d988557c3f38c
rhtweedie <rhtweedie12@gmail.com>
2022-09-15 22:34:14 +0100

    Add arguments length check

--- PrintBooks.java
+++ PrintBooks.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 public class PrintBooks {
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage:");
+            System.err.println("  PrintBooks <path of file containing books to be printed>");
+            System.exit(1);
+        }
+
         BookInterface database = new TextFileBookDatabase();
         ArrayList<Book> books = database.getAllBooks(args[0]);
         System.out.println("--- All books contained in database ---");
