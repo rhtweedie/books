42add624a8957b0e3777be02f6509198c5968b6b
Heather Tweedie <rhtweedie12@gmail.com>
2022-10-07 21:29:46 +0100

    Start on CLI

---
+++ src/rht/bookdb/BookCommander.java
@@ -0,0 +1,39 @@
+package rht.bookdb;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BookCommander {
+
+    public static void main(String args[]) {
+        try {
+            BookInterface bookDB = new SQLBookDB("books.sqlite");
+
+            System.out.println("What would you like to do today?");
+            System.out.println("1: List all books");
+
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    listAllBooks(bookDB);
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void listAllBooks(BookInterface bookDB) throws IOException, SQLException {
+        ArrayList<Book> books = bookDB.getAllBooks();
+        for (Book book : books) {
+            System.out.println(book.getTitle() + " by " + book.getAuthor());
+        }
+    }
+}
